9f945c0ebe77a3d3625ac8af9974f851143ad35a
tjmccoy <144307884+tjmccoy@users.noreply.github.com>
2024-01-14 21:06:40 -0600

    Implemented multiple classes (DD, EC, F, GU). (#8)
    * Implemented multiple classes (DD, EC, F, GU).
    
    * Made fixes to batch.

--- models/global_upgrade.py
+++ models/global_upgrade.py
@@ -1,4 +1,15 @@
 from models.common import WarframeObj
+from collections import defaultdict
 
 class GlobalUpgrade(WarframeObj):
-    pass
+    def __init__(self, json_global_upgrade: dict):
+        json_global_upgrade = defaultdict(lambda : None, json_global_upgrade)
+        self.start = json_global_upgrade['start']
+        self.end = json_global_upgrade['end']
+        self.upgrade = json_global_upgrade['upgrade']
+        self.operation = json_global_upgrade['operation']
+        self.operation_symbol = json_global_upgrade['operationSymbol']
+        self.upgrade_operation_value = json_global_upgrade['upgradeOperationSymbol']
+        self.expired = json_global_upgrade['expired']
+        self.eta = json_global_upgrade['eta']
+        self.desc = json_global_upgrade['desc']
