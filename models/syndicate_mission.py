cabb7f00549b79471548c1cc5ea8a2fc9390ba75
tyler <tylerjonahmccoy@gmail.com>
2024-01-18 23:57:27 -0600

    Implemented syndicate mission

--- models/syndicate_mission.py
+++ models/syndicate_mission.py
@@ -1,4 +1,14 @@
-from models.common import WarframeObj
+from collections import defaultdict
+from dateutil.parser import isoparse
+from models.common import WarframeObj, Job
 
 class SyndicateMission(WarframeObj):
-    pass
+    def __init__(self, json_syndicate: dict):
+        json_syndicate = defaultdict(lambda : None, json_syndicate)
+        self.nodes = json_syndicate['nodes'] if 'nodes' in json_syndicate else []
+        self.eta = json_syndicate['eta']
+        self.jobs = [Job(job) for job in json_syndicate['jobs']] if 'jobs' in json_syndicate else []
+        self.syndicate = json_syndicate['syndicate']
+        self.id = json_syndicate['id']
+        self.expiry = json_syndicate['expiry']
+        self.activation = isoparse(json_syndicate['activation']) if 'activation' in json_syndicate else None
