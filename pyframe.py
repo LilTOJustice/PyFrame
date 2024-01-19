cd8479dc79ca1dc570824f810430fac0bcd6076f
tjmccoy <144307884+tjmccoy@users.noreply.github.com>
2024-01-18 23:16:30 -0600

    Implemented Steel Path (#11)
    * Implemented Steel Path
    
    * Addressed PR comments.

--- pyframe.py
+++ pyframe.py
@@ -14,6 +14,7 @@ from models.fissure import Fissure
 from models.global_upgrade import GlobalUpgrade
 from models.news import News
 from models.sortie import Sortie
+from models.steel_path import SteelPath
 
 class APIException(Exception):
     pass
@@ -66,6 +67,9 @@ def get_news(platform: str = "pc") -> list[News]:
 def get_sortie(platform: str = "pc") -> list[Sortie]:
     return Sortie(get(platform, "sortie"))
 
+def get_steel_path(platform: str = "pc") -> SteelPath:
+    return SteelPath(get(platform, "steelPath"))
+
 if __name__ == "__main__":
     print("Alerts:")
     print(get_alerts())
@@ -91,3 +95,5 @@ if __name__ == "__main__":
     print(get_global_upgrades())
     print("\nSortie:")
     print(get_sortie())
+    print("\nSteel Path") 
+    print(get_steel_path())
