341b19b6c87edfa55b568984d7e29c9ac25715eb
LilTOJustice <muianick4@gmail.com>
2024-01-18 22:01:12 -0800

    Implemented Simaris

--- models/simaris.py
+++ models/simaris.py
@@ -1,4 +1,9 @@
+from collections import defaultdict
 from models.common import WarframeObj
 
 class Simaris(WarframeObj):
-    pass
+    def __init__(self, json_simaris: dict):
+        json_simaris = defaultdict(lambda : None, json_simaris)
+        self.target = json_simaris['target']
+        self.is_target_active = json_simaris['isTargetActive']
+        self.as_string = json_simaris['asString']
