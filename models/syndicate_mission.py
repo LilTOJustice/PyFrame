3b79a7eaed133d556894712f32d89a857345eece
tjmccoy <tylerjonahmccoy@gmail.com>
2024-01-20 13:21:17 -0600

    Resolved PR Comments.

--- models/syndicate_mission.py
+++ models/syndicate_mission.py
@@ -3,12 +3,12 @@ from dateutil.parser import isoparse
 from models.common import WarframeObj, Job
 
 class SyndicateMission(WarframeObj):
-    def __init__(self, json_syndicate: dict):
-        json_syndicate = defaultdict(lambda : None, json_syndicate)
-        self.nodes = json_syndicate['nodes'] if 'nodes' in json_syndicate else []
-        self.eta = json_syndicate['eta']
-        self.jobs = [Job(job) for job in json_syndicate['jobs']] if 'jobs' in json_syndicate else []
-        self.syndicate = json_syndicate['syndicate']
-        self.id = json_syndicate['id']
-        self.expiry = json_syndicate['expiry']
-        self.activation = isoparse(json_syndicate['activation']) if 'activation' in json_syndicate else None
+    def __init__(self, json_syndicate_mission: dict):
+        json_syndicate = defaultdict(lambda : None, json_syndicate_mission)
+        self.nodes = json_syndicate_mission['nodes'] if 'nodes' in json_syndicate_mission else []
+        self.eta = json_syndicate_mission['eta']
+        self.jobs = [Job(job) for job in json_syndicate_mission['jobs']] if 'jobs' in json_syndicate_mission else []
+        self.syndicate = json_syndicate_mission['syndicate']
+        self.id = json_syndicate_mission['id']
+        self.expiry = isoparse(json_syndicate_mission['expiry']) if 'expiry' in json_syndicate_mission else None
+        self.activation = isoparse(json_syndicate_mission['activation']) if 'activation' in json_syndicate_mission else None
