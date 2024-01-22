2e3ae6013f5edad85c89efa8f5e5196be810f402
tjmccoy <144307884+tjmccoy@users.noreply.github.com>
2024-01-21 21:13:02 -0600

    Tjmccoy/Vallis Cycle & Syndicate Mission (#19)
    * Implemented syndicate mission
    
    * Resolved PR Comments.
    
    * Implemented Vallis Cycle.
    
    * Addressed PR comments.

--- models/syndicate_mission.py
+++ models/syndicate_mission.py
@@ -1,4 +1,14 @@
-from models.common import WarframeObj
+from collections import defaultdict
+from dateutil.parser import isoparse
+from models.common import WarframeObj, Job
 
 class SyndicateMission(WarframeObj):
-    pass
+    def __init__(self, json_syndicate_mission: dict):
+        json_syndicate_mission = defaultdict(lambda : None, json_syndicate_mission)
+        self.nodes = json_syndicate_mission['nodes'] if 'nodes' in json_syndicate_mission else []
+        self.eta = json_syndicate_mission['eta']
+        self.jobs = [Job(job) for job in json_syndicate_mission['jobs']] if 'jobs' in json_syndicate_mission else []
+        self.syndicate = json_syndicate_mission['syndicate']
+        self.id = json_syndicate_mission['id']
+        self.expiry = isoparse(json_syndicate_mission['expiry']) if 'expiry' in json_syndicate_mission else None
+        self.activation = isoparse(json_syndicate_mission['activation']) if 'activation' in json_syndicate_mission else None
--- models/vallis_cycle.py
+++ models/vallis_cycle.py
@@ -1,4 +1,11 @@
+from collections import defaultdict
 from models.common import WarframeObj
+from dateutil.parser import isoparse
 
 class VallisCycle(WarframeObj):
-    pass
+    def __init__(self, json_vallis_cycle : dict):
+        json_vallis_cycle = defaultdict(lambda : None, json_vallis_cycle)
+        self.id = json_vallis_cycle['id']
+        self.expiry = isoparse(json_vallis_cycle['expiry']) if 'expiry' in json_vallis_cycle else None
+        self.time_left = json_vallis_cycle['timeLeft']
+        self.is_warm = json_vallis_cycle['isWarm']
