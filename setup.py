4a89a04340db3b26e501d8b0fa031371da4a7c1c
LilTOJustice <muianick4@gmail.com>
2024-01-23 01:16:41 -0800

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ required = ["python-dateutil"]
 
 setuptools.setup(
     name="python-PyFrame",
-    version="1.0.4",
+    version="1.0.5",
     author="LilTOJustice",
     author_email="muianick4@gmail.com",
     license=liscense,
